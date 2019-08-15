a329152b1ca6a5a119490eb9991d0ee5989f24a6
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-08-15 06:21:48 +0200

    Install Administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :posts
+    resources :users
+    root to: "posts#index"
+  end
+
   resources :posts
   devise_for :users
   root to: 'home#index'
