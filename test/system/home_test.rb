334f64920f3fcf07526db5eb689900028644cc31
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-08-17 19:46:31 +0200

    Improve home tests

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -3,26 +3,40 @@ require "application_system_test_case"
 class HomeTest < ApplicationSystemTestCase
   def setup
     @user = users(:user)
-    sign_in @user
   end
 
-  test "visiting the index by guest user" do
-    sign_out @user
+  test "visiting the index by guest" do
     visit root_url
     assert_text I18n.t("devise.failure.unauthenticated")
     assert_selector "h1", text: "Time Tracker"
-    assert_no_selector "nav a", text: "Home"
+    assert_no_selector "nav"
   end
 
-  test "visiting the index by logged in user" do
+  test "visiting the index" do
+    sign_in @user
     visit root_url
     assert_selector "h1", text: "Time Tracker"
-    assert_selector "nav a", text: "Home"
-    assert_selector "nav a", text: "Time Entries"
-    assert_selector "nav a", text: "Account"
+  end
 
+  test "navigating to home" do
+    sign_in @user
+    visit root_url
+    find("nav").click_link "Home"
+    assert_current_path root_path
+  end
+
+  test "navigating to posts" do
+    sign_in @user
+    visit root_url
+    find("nav").click_link "Time Entries"
+    assert_current_path posts_path
+  end
+
+  test "logging out" do
+    sign_in @user
+    visit root_url
     click_on "Account"
-    assert_selector "nav a", text: "Dashboard"
-    assert_selector "nav a", text: "Logout"
+    click_on "Logout"
+    assert_text I18n.t("devise.failure.unauthenticated")
   end
 end
