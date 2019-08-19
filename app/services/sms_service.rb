f091b7bf29b4cbf17087750b63d5cc24af0e8cff
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-08-19 21:41:50 +0200

    Add sms service

---
+++ app/services/sms_service.rb
@@ -0,0 +1,16 @@
+class SmsService
+  TWILIO_ACCOUNT_SID = Rails.application.credentials[Rails.env.to_sym][:twilio][:account_sid]
+  TWILIO_AUTH_TOKEN = Rails.application.credentials[Rails.env.to_sym][:twilio][:auth_token]
+  TWILIO_NUMBER = Rails.application.credentials[Rails.env.to_sym][:twilio][:number]
+
+  class << self
+    @@client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(message, to:)
+      @@client.messages.create from: TWILIO_NUMBER, to: to, body: message
+    rescue Twilio::REST::RestError => error
+      Rails.logger.error error.full_message
+      false
+    end
+  end
+end
---
+++ test/services/sms_service_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SmsServiceTest < ActiveSupport::TestCase
+  test "should send sms" do
+    assert SmsService.send_sms("message", to: "+15005550006")
+  end
+
+  test "should not send sms to an invalid number" do
+    assert_not SmsService.send_sms("message", to: "+15005550001")
+  end
+end
