886d72b1514c8bac3a9eff7362721327eae19734
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-08-27 13:30:39 +0200

    Sort audit logs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
       @submitted_posts = Post.submitted.includes(:user)
       @recent_audit_logs = AuditLog.last(10)
     else
-      @pending_audit_logs = current_user.audit_logs.pending
+      @pending_audit_logs = current_user.audit_logs.pending.order(start_date: :desc)
     end
   end
 end
